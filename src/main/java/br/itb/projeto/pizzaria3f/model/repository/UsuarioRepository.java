0fa6ad2d596df5e9c4d19cea2146bf86373371b7
Izabella da Silva Oliveira <contato.bellaiza@gmail.com>
2024-04-02 07:50:49 -0300

    first commit

---
+++ src/main/java/br/itb/projeto/pizzaria3f/model/repository/UsuarioRepository.java
@@ -0,0 +1,18 @@
+package br.itb.projeto.pizzaria3f.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.itb.projeto.pizzaria3f.model.entity.Usuario;
+
+@Repository
+public interface UsuarioRepository extends 
+			  JpaRepository<Usuario, Long>{
+
+}
+
+
+
+
+
+
